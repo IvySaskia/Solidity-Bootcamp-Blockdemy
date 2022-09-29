ef89d473825a581eaed71f132617fdf1dc61e2f7
Remix Workspace <>
2022-09-29 10:59:28 -0400

    ADD: WIP TipoValor

---
+++ contracts/DataTypesValor.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TipoValor{
+
+    //Booleanos
+    bool flagVerdadero = true;
+    bool flagFalso = false;
+
+
+    //Entero con signo - Positivos y Negativos
+
+    //256 bits
+
+    int numero1 = 2;
+    int numero2 = -3;
+
+
+    //Entero sin signo - Positivos
+
+    int numero3 = 2;
+    int numero4 = -4;
+
+
+    //
+
+
+
+}
